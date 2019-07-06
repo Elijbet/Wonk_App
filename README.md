de1c5c702e282e8a814d27bdf41ce47ee9ee5819
Eliza Khachatryan <elijbet@gmail.com>
2019-07-06 11:55:23 -0700

    get books from NYT books on the best sellers, append last 3 years - have a default populated view - a description on the flipped back of the card

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# WONK 
+# WONK APP
 
-> Wonk is an app for bookworms to shuffle your bookshelves based on reading progress. It also a tool to wonk out on specific books: get all the podcast, events, news, as well as find out whther it has been adopted into a movie. Reworking the Goodreads API Vue app done a couple of years ago with new skills and features.
+> Wonk is an app for bookworms to geek out on their books. You can shuffle your bookshelves based on reading progress. It also a tool to wonk out on specific books: get all the podcast, events, news, as well as find out whther it has been adopted into a movie. Reworking the Goodreads API Vue app done a couple of years ago with new skills and features.
  
 ___
 
@@ -20,18 +20,28 @@ ___
 
 - masonry layout of book results, very little margins
 - flip card animation applied to my layout
-- fade in animation applied to backside text
+- fade in animation applied to backside text, along with a black and white filter and shade cast.
 
-- get books from goodreads api
-- have a default populated vue, see what categories are available from api- 
-- make search work
+- get books from NYT books on the best sellers, append last 3 years
+- have a default populated view
+- a description on the flipped back of the card
 
-### SEARCH BAR
+### HIDE SECRET KEYS
+
+- create a file called .env in the root of your project's directory
+- inside the .env file, prepend VUE_APP_title to your API key name of choice and assign it
+- add the .env file to your .gitignore file
+- access the API key via the process.env object
+- [vue docs](https://cli.vuejs.org/guide/mode-and-env.html#using-env-variables-in-client-side-code)
+
+#### SEARCH BAR
 
 - search bar mimic google
 - mimic unfolded history from google search 
 - save last 10 items of history search
 - list recent first
+- make search work
+
 ___
 
 ## Project setup
--- src/components/MasonryBookResults.vue
+++ src/components/MasonryBookResults.vue
@@ -1,127 +1,61 @@
 <template>
   <div class="masonry-wrapper">
     <div class="masonry">
-
-      <div class="card-wrapper">
+    
+      <div class="card-wrapper" v-for="book in books">
         <div 
           class="masonry-item card" 
           v-on:click="flip"
-          :class="[toggleOn ? 'transform' : '']">
+          :class="[toggleOn ? '' : 'rotate']">
           <div :class="[toggleOn ? 'displayOn' : 'displayOff']" class="front">
             <img 
-              src="https://picsum.photos/450/325?image=100" 
+              :src="book.book_image" 
+              width="300px"
               alt="Dummy Image" 
               class="masonry-content fadeIn"
             >
           </div>
-          <div :class="[toggleOn ? 'displayOff' : 'displayOn']" class="back">
+          <div :class="[toggleOn ? 'displayOff' : 'displayOn']" class="back rotate">
             <img 
-              src="https://picsum.photos/450/325?image=100" 
+              :src="book.book_image" 
+              width="300px"
               alt="Dummy Image" 
-              class="masonry-content rotate"
+              class="masonry-content"
             >
-            <div class="position-text fadeIn">Card Flipped</div>
+            <div class="position-text fadeIn ">{{book.description}}</div>
             <div class="overlay" />
           </div>
         </div>
       </div>
 
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/450?image=200" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/280?image=300" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/540?image=400" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/380?image=500" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/300?image=600" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/400?image=700" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/300?image=800" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/280?image=900" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/480?image=925" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/550?image=950" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/600?image=1000" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/325?image=25" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/450?image=50" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/280?image=75" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/540?image=100" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/380?image=125" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/300?image=161" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/400?image=175" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/300?image=200" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/280?image=225" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/480?image=250" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/550?image=275" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/600?image=300" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/325?image=13" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/450?image=26" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/280?image=39" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/540?image=52" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/380?image=65" alt="Dummy Image" class="masonry-content">
-      </div>
-      <div class="masonry-item">
-        <img src="https://picsum.photos/450/300?image=78" alt="Dummy Image" class="masonry-content">
-      </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
   export default {
     data(){
       return{
-        toggleOn: true
+        toggleOn: true,
+        books: []
+      }
+    },
+    async created(){
+      try {
+        const response = await axios.get(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=1HDYBZa5JVVORNQ13hq5vuPPGG7f1KjU`)
+        const secondResponse = await axios.get(`https://api.nytimes.com/svc/books/v3/lists/2018-01-01/hardcover-fiction.json?api-key=1HDYBZa5JVVORNQ13hq5vuPPGG7f1KjU`)
+        const thirdResponse = await axios.get(`https://api.nytimes.com/svc/books/v3/lists/2017-01-01/hardcover-fiction.json?api-key=1HDYBZa5JVVORNQ13hq5vuPPGG7f1KjU`)
+
+        this.books = response.data.results.books
+        this.books.push(... secondResponse.data.results.books)
+        this.books.push(... thirdResponse.data.results.books)
+
+        console.log('this.books', this.books)
+      } catch (e) {
+        console.log('e', e)
+        // this.errors.push(e)
       }
     },
     methods: {
@@ -240,18 +174,18 @@ html {
 }
 .masonry-wrapper {
   padding: 1.5em;
-  max-width: 1500px;
+  max-width: 1200px;
   margin-right: auto;
   margin-left: auto;
 }
 .masonry {
   columns: 1;
-  column-gap: 5px;
+  column-gap: 15px;
 }
 .masonry-item {
   display: inline-block;
   vertical-align: top;
-  margin-bottom: 5px;
+  margin-bottom: 15px;
 }
 @include large-screen {  
 .masonry {
@@ -303,15 +237,15 @@ html {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  margin: 0 !important;
+  padding: 0 !important;
 }
 .card-wrapper {
-  perspective:800px;
-}
-.transform {
-  transform: rotateY(-180deg);
+  perspective: 800px;
 }
 .back {
   filter: grayscale(100%);
+  opacity: 0.7;
 }
 .overlay { 
   position: absolute; 
@@ -319,6 +253,6 @@ html {
   left: 0; 
   right:0; 
   bottom:0; 
-  background-color: rgba(0,0,0,0.5); 
+  background-color: rgba(0,0,0,0.8); 
 }
 </style>
